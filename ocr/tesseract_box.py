2aa3f88afeea02cb70cb321c34569e5c2180cb05
eudvazquez <daniel.v@thoughtworks.com>
2019-09-19 10:54:47 +0200

    Finish clustering wiht a set of naive tests

--- ocr/tesseract_box.py
+++ ocr/tesseract_box.py
@@ -8,4 +8,29 @@ class TesseractBox:
         self.height = int(height)
         self.text = text
 
+    def horizontal_distance(self, other_box):
+        if other_box.left >= self.left:
+            return  abs((self.left + self.width) - other_box.left)
+        else:
+            return  abs((other_box.left + other_box.width) - self.left)
+
+    def join_horizontally(self, other_box):
+        new_end = other_box.left + other_box.width
+        new_width = new_end - self.left
+        self.text = self.text + ' '+ other_box.text
+        self.width = new_width
+
+    def vertical_distance(self, other_box):
+        if other_box.top >= self.top:
+            return  abs((self.top + self.height) - other_box.top)
+        else:
+            return  abs((other_box.top + other_box.height) - self.top)
+
+    def join_vertically(self, other_box):
+        new_end = other_box.top + other_box.height
+        new_height = new_end - self.top
+        self.text = self.text + '\n'+ other_box.text
+        self.height = new_height
+
+
 
