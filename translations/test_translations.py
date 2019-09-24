f298a2c6d2d9924cda041475fe2244d66945877f
eudvazquez <daniel.v@thoughtworks.com>
2019-09-24 11:54:31 +0200

    preparing testing

---
+++ translations/test_translations.py
@@ -0,0 +1,9 @@
+
+import unittest
+from ocr.tesseract import Tesseract
+
+class Test_Tesseract(unittest.TestCase):
+
+    def test_join_paragraphs_happy_path(self):
+        self.assertEqual(1, 1)
+
