5f5f3f0e7859a4486a8c4d39cbc7698bcebf10c0
eudvazquez <daniel.v@thoughtworks.com>
2019-10-10 17:05:54 +0200

    added the processor to the test. Its obvious that the translation boxes are not correc

--- tesseract_test.py
+++ tesseract_test.py
@@ -2,15 +2,31 @@ import pytesseract
 from ocr.tesseract_parser import TesseractParser
 from ocr.tesseract_cluster import TesseractCluster
 from pdf.converter import Converter
-from PIL import Image
+from PIL import Image, ImageEnhance
 from image.image_processor import ImageProcessor
 
 converter = Converter()
 
 converter.to_jpg('./data/Guild/Stat Cards/M3E_Gld_Arc_Augmented_CharlesHoffman.pdf','./data/image/Faux')
-parser = TesseractParser(50)
-raw_data = parser.get_tesseract_boxes('./data/image/Faux/out-1.jpg')
-cluster = TesseractCluster(60, 34)
+
+
+
+
+im = Image.open('./data/image/Faux/out-1.jpg')
+enh = ImageEnhance.Sharpness(im)
+im = enh.enhance(2)
+
+# enh = ImageEnhance.Contrast(im)
+# im = enh.enhance(2)
+
+im.save('./data/image/Faux/out-1-post.jpg')
+
+
+
+
+parser = TesseractParser(70)
+raw_data = parser.get_tesseract_boxes('./data/image/Faux/out-1-post.jpg')
+cluster = TesseractCluster(50, 34)
 translation_boxes = cluster.cluster_boxes(raw_data)
 for translation_box in translation_boxes:
     print(translation_box)
